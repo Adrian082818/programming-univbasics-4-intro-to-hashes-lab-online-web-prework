fb45d27bd6285e06d593398ddd151169a4eb5c7b
Adrian Mohabir <mohabiradrian@gmail.com>
2019-11-03 15:07:20 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -15,7 +15,7 @@ def id_generator
 end
 
 def my_hash_creator(key, value)
-  my_hash_creator = {:name => "key", :value => "value"}
+  my_hash_creator = {:name => "name", :value => "Grace Hopper"}
 end
 
 def read_from_hash(hash, key)
